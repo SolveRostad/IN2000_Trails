1e522b2139b3eb1c180b3e0c76dc05b7141af5a6
sondrein <sondrein@ifi.uio.no>
2025-03-12 14:34:45 +0100

    Fetch MET Locationforecast

---
+++ app/src/main/java/no/uio/ifi/in2000/sondrein/in2000_gruppe3/data/LocationForecastAPI/datasource/FetchWeatherData.kt
@@ -0,0 +1,35 @@
+package no.uio.ifi.in2000.sondrein.in2000_gruppe3.data.LocationForecastAPI.datasource
+
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+
+// Get temperature at selected place right now
+fun getTemperature(lat: Double, lon: Double): String {
+    val urlString = "https://api.met.no/weatherapi/locationforecast/2.0/classic?lat=$lat&lon=$lon"
+
+    return try {
+        val url = URL(urlString)
+        val connection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("User-Agent", "IN2000_TripPlanner/1.0 (sondrein@uio.no)")
+        connection.connect()
+
+        val responseCode = connection.responseCode
+        val responseText = connection.inputStream.bufferedReader().use { it.readText() }
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            val json = JSONObject(responseText)
+            val temp = json.getJSONObject("properties")
+                .getJSONArray("timeseries").getJSONObject(0)
+                .getJSONObject("data").getJSONObject("instant")
+                .getJSONObject("details").getDouble("air_temperature")
+            "$temp°C"
+        } else {
+            "Wrong connection: ${connection.responseCode}"
+        }
+    } catch (e: Exception) {
+        "Feil ved henting av værdata: ${e.message}"
+    }
+}
