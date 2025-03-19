4a27ad60044126b6b65dc8764cd67e12ea094e7d
sondrein <sondrein@ifi.uio.no>
2025-03-19 10:32:32 +0100

    Fornøyd nå lennard?

--- app/src/main/java/no/uio/ifi/in2000/sondrein/in2000_gruppe3/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/sondrein/in2000_gruppe3/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.navigation.AppNavHost
 import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.screens.favoriteScreen.FavoriteScreen
 import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.screens.hikeCard.HikeCard
 import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.screens.home.HomeScreen
@@ -25,72 +26,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             IN2000_gruppe3Theme {
                 Surface {
-                    App()
+                    AppNavHost()
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun App() {
-    val navController = rememberNavController()
-    AppNavHost(navController = navController)
-}
-
-@Composable
-fun AppNavHost(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = Screen.Home.route
-    ) {
-        // Home screen
-        composable(Screen.Home.route) {
-            HomeScreen(
-                onHikeClick = { hikeId ->
-                    navController.navigate(Screen.HikeCard.createRoute(hikeId))
-                },
-                onFavoritesClick = {
-                    navController.navigate(Screen.Favorites.route)
-                },
-                navController = navController
-            )
-        }
-
-        // HikeCard screen
-        composable(
-            route = Screen.HikeCard.route,
-            arguments = listOf(navArgument("hikeId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val hikeId = backStackEntry.arguments?.getInt("hikeId") ?: -1
-            HikeCard(
-                hikeId = hikeId,
-                onBackClick = {
-                    navController.popBackStack()
-                },
-                onFavoriteClick = {
-                    navController.navigate(Screen.Favorites.route) {
-                        // Pop up to Home screen, but don't include it
-                        popUpTo(Screen.Home.route) { inclusive = false }
-                    }
-                }
-            )
-        }
-
-        // Favorites screen
-        composable(Screen.Favorites.route) {
-            FavoriteScreen(
-                onHikeClick = { hikeId ->
-                    navController.navigate(Screen.HikeCard.createRoute(hikeId))
-                },
-                onHomeClick = {
-                    navController.navigate(Screen.Home.route) {
-                        // Pop up to start destination and include it
-                        popUpTo(Screen.Home.route) { inclusive = true }
-                    }
-                },
-                navController = navController
-            )
         }
     }
 }
