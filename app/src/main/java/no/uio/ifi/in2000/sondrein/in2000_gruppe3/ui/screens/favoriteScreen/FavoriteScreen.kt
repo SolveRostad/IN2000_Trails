e0518bf78e3d0e4f3707ac92234a27bf98927571
sondrein <sondrein@ifi.uio.no>
2025-03-19 12:56:34 +0100

    Data i smallHikeCard

--- app/src/main/java/no/uio/ifi/in2000/sondrein/in2000_gruppe3/ui/screens/favoriteScreen/FavoriteScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/sondrein/in2000_gruppe3/ui/screens/favoriteScreen/FavoriteScreen.kt
@@ -3,23 +3,14 @@ package no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.screens.favoriteScreen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -27,7 +18,6 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.navigation.BottomBar
 import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.screens.Hike
-import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.screens.hikeCard.SmallHikeCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -78,9 +68,9 @@ fun FavoriteScreen(
                 contentPadding = PaddingValues(16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                items(favoriteHikes) { hike ->
-                    SmallHikeCard(hike = hike, onClick = { onHikeClick(hike.id) })
-                }
+//                items(ui) { hike ->
+//                    SmallHikeCard(hike = hike, onClick = { onHikeClick(hike.id) })
+//                }
             }
         }
     }
