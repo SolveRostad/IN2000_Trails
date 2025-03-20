e8f782fa3a706d3330be0ac901bc9a9faeef547b
sondrein <sondrein@ifi.uio.no>
2025-03-20 16:51:30 +0100

    Endret måten søkefeltet og forslag til steder ser ut

--- app/src/main/java/no/uio/ifi/in2000/sondrein/in2000_gruppe3/ui/mapbox/SearchBarForMap.kt
+++ app/src/main/java/no/uio/ifi/in2000/sondrein/in2000_gruppe3/ui/mapbox/SearchBarForMap.kt
@@ -7,10 +7,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.MaterialTheme
@@ -24,17 +24,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import com.mapbox.search.autocomplete.PlaceAutocomplete
 import com.mapbox.search.autocomplete.PlaceAutocompleteSuggestion
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.sondrein.in2000_gruppe3.R
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import no.uio.ifi.in2000.sondrein.in2000_gruppe3.ui.screens.homeScreen.HomeScreenViewModel
 
 @Composable
 fun SearchBarForMap(viewModel: HomeScreenViewModel) {
     val uiState by viewModel.homeScreenUIState.collectAsState()
     val coroutineScope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
     val suggestions = remember { mutableStateOf<List<PlaceAutocompleteSuggestion>>(emptyList()) }
 
     // Create autocomplete client
@@ -42,53 +64,71 @@ fun SearchBarForMap(viewModel: HomeScreenViewModel) {
         PlaceAutocomplete.create()
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(0.dp, 30.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        // Search field
-        OutlinedTextField(
-            value = uiState.searchQuery,
-            onValueChange = { newQuery ->
-                viewModel.updateSearchQuery(newQuery)
-
-                // Search for places when query changes
-                coroutineScope.launch {
-                    if (newQuery.length >= 3) {
-                        val response = placeAutocomplete.suggestions(newQuery)
-
-                        if (response.isValue) {
-                            suggestions.value = response.value ?: emptyList()
-                        } else {
-                            Log.e("SearchBar", "Error fetching suggestions", response.error)
-                        }
-                    } else {
-                        suggestions.value = emptyList()
-                    }
-                }
-            },
+    Column {
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
-            placeholder = { Text("Search for a place") },
-            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search") }
-        )
+                .padding(top = 10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            // Search field
+            TextField(
+                value = uiState.searchQuery,
+                onValueChange = { newQuery ->
+                    viewModel.updateSearchQuery(newQuery)
+
+                    // Search for places when query changes
+                    coroutineScope.launch {
+                        if (newQuery.length >= 3) {
+                            val response = placeAutocomplete.suggestions(newQuery)
+
+                            if (response.isValue) {
+                                suggestions.value = response.value ?: emptyList()
+                            } else {
+                                Log.e("SearchBar", "Error fetching suggestions", response.error)
+                            }
+                        } else {
+                            suggestions.value = emptyList()
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(30.dp))
+                    .border(1.dp, Color.Gray, RoundedCornerShape(30.dp))
+                    .onKeyEvent { keyEvent ->
+                        if (keyEvent.type == KeyEventType.KeyUp && keyEvent.key == Key.Enter) {
+                            keyboardController?.hide()
+                            suggestions.value = emptyList()
+                            // Handle search
+                        }
+                        true
+                    },
+                singleLine = true,
+                placeholder = { Text("Search for a place") },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search") }
+            )
+        }
 
         // Show suggestions
         if (suggestions.value.isNotEmpty()) {
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp, 70.dp, 16.dp, 0.dp)
-                    .background(MaterialTheme.colorScheme.background)
+                    .background(MaterialTheme.colorScheme.background, RoundedCornerShape(10.dp))
+                    .border(1.dp, Color.Gray, RoundedCornerShape(10.dp))
             ) {
                 items(suggestions.value) { suggestion ->
                     Text(
                         text = suggestion.name,
                         modifier = Modifier
                             .fillMaxWidth()
+                            .padding(10.dp)
                             .clickable {
                                 // Handle selection
                                 coroutineScope.launch {
@@ -103,10 +143,14 @@ fun SearchBarForMap(viewModel: HomeScreenViewModel) {
                                 }
                                 // Clear suggestions
                                 suggestions.value = emptyList()
+                                keyboardController?.hide()
                             }
-                            .padding(16.dp)
                     )
-                    Divider()
+                    Divider(
+                        modifier = Modifier.fillMaxWidth(),
+                        color = Color.Gray,
+                        thickness = 1.dp
+                    )
                 }
             }
         }
