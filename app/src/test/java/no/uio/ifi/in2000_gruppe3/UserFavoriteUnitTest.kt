710f3ea2d2ed248db34224dcc6904742b40b6578
lennarddenby <lennard.rolstad.denby@gmail.com>
2025-04-10 11:08:36 +0200

    fylte inn body for user UnitTests

--- app/src/test/java/no/uio/ifi/in2000_gruppe3/UserFavoriteUnitTest.kt
+++ app/src/test/java/no/uio/ifi/in2000_gruppe3/UserFavoriteUnitTest.kt
@@ -4,24 +4,60 @@ import org.junit.Test
 
 
 class UserFavoriteUnitTest {
+    val userRepository = TODO()
+    val userViewModel = TODO()
+
+    //Dummy data til testene
+    val bruker1 = User(username = "Aanund")
+    val bruker2 = User(username = "Victor")
+
     @Test
     fun testAddUser() {
-        //TODO test for å legge til bruker
+        println("Tester å legge til bruker: $bruker1")
+
+        userRepository.addUser(bruker1)
+        assertContains(userRepository.getAllUsers(), bruker1, "Bruker ble ikke lagt til")
+
+        println("---testAddUser PASSERT---")
     }
 
     @Test
     fun testDeleteUser() {
-        //TODO test for å slette bruker
+        println("Tester å slette bruker: $bruker1")
+
+        userRepository.addUser(bruker1)
+        userRepository.deleteUser(bruker1)
+        assertFalse(userRepository.getAllUsers().contains(bruker1), "Bruker ble ikke slettet")
+
+        println("---testDeleteUser PASSERT---")
     }
 
     @Test
     fun testGetAllUsers() {
-        //TODO test for å hente alle brukere
+        println("tester å hente alle brukere")
+
+        userRepository.addUser(bruker1)
+        userRepository.addUser(bruker2)
+
+        val allUsers = userRepository.getAllUsers()
+
+        assertContains(allUsers, bruker1, "Bruker 1 ble ikke lagt til")
+        assertContains(allUsers, bruker2, "Bruker 2 ble ikke lagt til")
+
+        println("---testGetAllUsers PASSERT---")
     }
 
     @Test
     fun testSelectUser() {
-        //TODO test for å velge bruker
+        println("Tester å velge bruker: $bruker1")
+
+        userRepository.addUser(bruker1)
+        userViewModel.selectUser(bruker1.username)
+
+        val selectedUser = userViewModel.getSelectedUser()
+        assertEquals(selectedUser, bruker1, "Bruker ble ikke valgt")
+
+        println("---testSelectUser PASSERT---")
     }
 
     @Test
