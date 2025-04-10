36f13d78fecdd265ebd5a1e122fdf6c06c4eb41d
lennarddenby <lennard.rolstad.denby@gmail.com>
2025-04-10 10:44:43 +0200

    Lagde test signaturer for testdreven utvikling for User/Favorite room database. Skal teste repository og viewmodel.

---
+++ app/src/test/java/no/uio/ifi/in2000_gruppe3/UserFavoriteUnitTest.kt
@@ -0,0 +1,41 @@
+package no.uio.ifi.in2000_gruppe3
+
+import org.junit.Test
+
+
+class UserFavoriteUnitTest {
+    @Test
+    fun testAddUser() {
+        //TODO test for å legge til bruker
+    }
+
+    @Test
+    fun testDeleteUser() {
+        //TODO test for å slette bruker
+    }
+
+    @Test
+    fun testGetAllUsers() {
+        //TODO test for å hente alle brukere
+    }
+
+    @Test
+    fun testSelectUser() {
+        //TODO test for å velge bruker
+    }
+
+    @Test
+    fun testAddFavorite() {
+        //TODO test for å legge til hikeID i favoritter
+    }
+
+    @Test
+    fun testDeleteFavorite() {
+        //TODO test for å fjerne hikeID fra favoritter
+    }
+
+    @Test
+    fun testGetAllFavorites() {
+        //TODO test for å hente alle favoritter
+    }
+}
