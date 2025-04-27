75f218f2b02b77818b63083fa460924176e2d2ac
solveros <solveros@ifi.uio.no>
2025-04-27 23:43:55 +0200

    was????

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     kotlin("plugin.serialization") version "2.1.10"
-    id("com.google.devtools.ksp")
+    id("com.google.devtools.ksp") version "2.1.10-1.0.30"
 }
 
 android {
