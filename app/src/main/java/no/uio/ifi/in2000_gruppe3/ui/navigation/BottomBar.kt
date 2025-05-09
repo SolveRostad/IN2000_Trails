cbb445b1a70dd4a70f8d6a64d081449edcc7fd54
sondrein <sondrein@ifi.uio.no>
2025-05-09 14:33:19 +0200

    keyboard action

--- app/src/main/java/no/uio/ifi/in2000_gruppe3/ui/navigation/BottomBar.kt
+++ app/src/main/java/no/uio/ifi/in2000_gruppe3/ui/navigation/BottomBar.kt
@@ -45,7 +45,7 @@ fun BottomBar(navController: NavHostController) {
                     },
                     modifier = Modifier
                         .weight(1f)
-                        .size(35.dp)
+                        .size(30.dp)
                 ) {
                     Icon(
                         imageVector = Icons.Default.Favorite,
@@ -81,7 +81,7 @@ fun BottomBar(navController: NavHostController) {
                     },
                     modifier = Modifier
                         .weight(1f)
-                        .size(35.dp)
+                        .size(30.dp)
                 ) {
                     Icon(
                         painter = painterResource(R.drawable.person),
